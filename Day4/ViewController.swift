e89cb3060f755aa0d19dd77a20433810966efab1
FritzHeider <fritz.heider@students.makeschool.com>
2020-01-28 09:37:39 -0800

    Initial Commit

---
+++ Day4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Day4
+//
+//  Created by Fritz Heider on 1/28/20.
+//  Copyright © 2020 Make School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
